697d94fc9a57e79ada64ed797ec270fd45e24a18
cceer <sisir.sital@gmail.com>
2022-07-17 10:19:31 +0545

    all files added

---
+++ server.js
@@ -0,0 +1,39 @@
+const next = require("next");
+
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+
+const dev = process.env.NODE_ENV !== "production";
+const server = next({ dev });
+const handle = server.getRequestHandler();
+
+const app = require("./server/app");
+
+dotenv.config({ path: "./config.env" });
+
+const db = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+server.prepare().then(() => {
+  app.get("*", (req, res) => {
+    return handle(req, res);
+  });
+  // connect mongoose here
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught exception: shutting down the app");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION: Shutting down the app");
+  server.close(() => {
+    process.exit(1);
+  });
+});
